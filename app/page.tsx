2dbe1492fa9f731879c3fe4bffd9d4a3d3e96282
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2024-11-19 16:47:26 +0900

    feat: 材料

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
+import { Box, Card, CardContent, Grid, Link, Typography } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Image from "next/image";
 
@@ -125,7 +125,7 @@ export default function Home() {
           display={"flex"}
           justifyContent={"center"}
           alignItems={"center"}
-          paddingInline={"20px"}
+          p={"20px"}
         >
           <Card sx={{ width: "275px" }}>
             <CardContent>
@@ -193,7 +193,7 @@ export default function Home() {
           材料を用意する
         </Typography>
         <Box width={"80%"} height={6} my={"8px"} bgcolor="#333333" />
-        <Box>
+        <Box mb={5}>
           <Typography variant="h4" fontSize={"28px"}>
             【レシピ】
           </Typography>
@@ -210,11 +210,11 @@ export default function Home() {
               paddingInline={"20px"}
               flexDirection={"column"}
             >
-              <Typography variant="h4" fontSize={"28px"}>
+              <Typography variant="h5" fontSize={"28px"}>
                 Arduino R4 WiFi
               </Typography>
               <Box paddingLeft={2}>
-                <Typography variant="h4" fontSize={"20px"}>
+                <Typography variant="h5" fontSize={"20px"}>
                   どのご家庭にもあるワンボードマイコン、ArduinoのWiFi搭載モデルです。Renesasの32ビットマイクロコントローラRA4M1をベースに設計されており、Wi-Fi®およびBluetooth®接続用のESP32-S3-MINI-1-N8が搭載されています。なお、ご家庭にESP-WROOM-32しかない場合はそちらを使用しても大丈夫です。
                 </Typography>
               </Box>
@@ -228,14 +228,74 @@ export default function Home() {
               paddingInline={"20px"}
               flexDirection={"column"}
             >
-              <Typography variant="h4" fontSize={"28px"}>
+              <Typography variant="h5" fontSize={"28px"}>
                 デジタル・マイクロサーボ SG90
               </Typography>
               <Box paddingLeft={2}>
-                <Typography variant="h4" fontSize={"20px"}>
+                <Typography variant="h5" fontSize={"20px"}>
                   どのご家庭にもあるサーボモーターです。切らしてしまっている場合には他のサーボモーターを使ってもかまいません。
                 </Typography>
               </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Typography variant="h4" fontSize={"28px"}>
+          ー材料を切らしてしまった場合ー
+        </Typography>
+        <Box display={"flex"} paddingInline={"20px"} paddingBottom={"10px"}>
+          <Image
+            src={"/akihabara.jpg"}
+            alt={"秋葉原駅"}
+            width={500}
+            height={0}
+          />
+          <Box
+            display={"flex"}
+            paddingInline={"20px"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+          >
+            <Typography variant="h5" fontSize={"32px"} fontWeight={"bold"}>
+              ①秋葉原に行きます
+            </Typography>
+          </Box>
+        </Box>
+        <Box display={"flex"} paddingInline={"20px"} paddingBottom={"10px"}>
+          <Image src={"/map.png"} alt={"マップ"} width={500} height={0} />
+          <Box
+            display={"flex"}
+            paddingInline={"20px"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+          >
+            <Typography variant="h5" fontSize={"32px"} fontWeight={"bold"}>
+              ②なんかいい感じに曲がります
+            </Typography>
+          </Box>
+        </Box>
+        <Box display={"flex"} paddingInline={"20px"} paddingBottom={"10px"}>
+          <Image
+            src={"/akiduki.png"}
+            alt={"秋月電子電商"}
+            width={500}
+            height={0}
+          />
+          <Box
+            display={"flex"}
+            paddingInline={"20px"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+          >
+            <Typography variant="h5" fontSize={"32px"} fontWeight={"bold"}>
+              ③ここで買ってください
+            </Typography>
+            <Box display={"flex"} flexDirection={"row"}>
+              <Typography variant="h5" fontSize={"15px"} fontWeight={"bold"}>
+                場所→
+              </Typography>
+              <Link href="https://www.google.com/maps/dir/%E6%9D%B1%E4%BA%AC%E9%83%BD%E5%8D%83%E4%BB%A3%E7%94%B0%E5%8C%BA%E5%A4%96%E7%A5%9E%E7%94%B0%EF%BC%91%E4%B8%81%E7%9B%AE+%E7%A7%8B%E8%91%89%E5%8E%9F%E9%A7%85/%E3%80%92101-0021+%E6%9D%B1%E4%BA%AC%E9%83%BD%E5%8D%83%E4%BB%A3%E7%94%B0%E5%8C%BA%E5%A4%96%E7%A5%9E%E7%94%B0%EF%BC%91%E4%B8%81%E7%9B%AE%EF%BC%98%E2%88%92%EF%BC%93+%E9%87%8E%E6%B0%B4%E3%83%93%E3%83%AB+1F+%E7%A7%8B%E6%9C%88%E9%9B%BB%E5%AD%90%E9%80%9A%E5%95%86+%E7%A7%8B%E8%91%89%E5%8E%9F%E5%BA%97/@35.6983781,139.7684964,904m/data=!3m2!1e3!5s0x60188c1c50dd801d:0xde1dbb4846f44c6b!4m14!4m13!1m5!1m1!1s0x60188ea7e2f93329:0x158f36257ff597b1!2m2!1d139.7730717!2d35.698383!1m5!1m1!1s0x60188c1c50deaaab:0x2017bd800004a2be!2m2!1d139.7698196!2d35.6993604!3e2?authuser=0&entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D">
+                https://www.google.com/maps/dir
+              </Link>
             </Box>
           </Box>
         </Box>
